b3ff0509dd495aa64f2e7320da2527e795e63803
lxuxiang <5b1g0901@stust.edu.tw>
2025-05-21 14:37:13 +0800

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";  // 路徑要對
+import viteLogo from "/vite.svg";
 import "./App.css";
 import MySlider from "./component/MySlider";
 import MyPanel from "./component/MyPanel";
