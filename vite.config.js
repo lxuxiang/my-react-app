43f90c68c16e2788d985abd019be20792a537297
lxuxiang <5b1g0901@stust.edu.tw>
2025-05-21 13:59:36 +0800

    111

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MY-REACT-APP/',  // <--- 記得改成你的 GitHub repo 名稱
+  base: '/my-react-app/',  // ⚠️ 必須加上你的 repo 名稱
 })
