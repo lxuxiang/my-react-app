8119b726bfbc453af5cd7421aecd3b765470de51
lxuxiang <5b1g0901@stust.edu.tw>
2025-05-21 14:38:26 +0800

    123

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>123456789陳小明</h1>
+      <h1>7777</h1>
       <h2>我的第一個元件：Slider</h2>
       <div>
         <p>R:<MySlider /></p>
