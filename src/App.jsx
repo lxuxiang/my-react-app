50330acb7bd385de0436e8fa3e7bf549b646df89
lxuxiang <5b1g0901@stust.edu.tw>
2025-05-21 14:24:50 +0800

    123

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>本網站使用Vite + React開發</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
